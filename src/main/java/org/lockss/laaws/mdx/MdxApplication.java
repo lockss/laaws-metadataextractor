97e7cc1c0651789cdf74bcc568b0049396fe4f1a
fergaloy <fergaloy@stanford.edu>
2018-03-06 10:03:31 -1000

    Switched to depend on the metadataextractor-common project.

--- src/main/java/org/lockss/laaws/mdx/MdxApplication.java
+++ src/main/java/org/lockss/laaws/mdx/MdxApplication.java
@@ -1,28 +1,32 @@
 /*
 
- Copyright (c) 2017-2018 Board of Trustees of Leland Stanford Jr. University,
- all rights reserved.
+Copyright (c) 2000-2018 Board of Trustees of Leland Stanford Jr. University,
+all rights reserved.
 
- Permission is hereby granted, free of charge, to any person obtaining a copy
- of this software and associated documentation files (the "Software"), to deal
- in the Software without restriction, including without limitation the rights
- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- copies of the Software, and to permit persons to whom the Software is
- furnished to do so, subject to the following conditions:
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
 
- The above copyright notice and this permission notice shall be included in
- all copies or substantial portions of the Software.
+1. Redistributions of source code must retain the above copyright notice, this
+list of conditions and the following disclaimer.
 
- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
- STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
- WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
- IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+2. Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions and the following disclaimer in the documentation and/or
+other materials provided with the distribution.
 
- Except as contained in this notice, the name of Stanford University shall not
- be used in advertising or otherwise to promote the sale, use or other dealings
- in this Software without prior written authorization from Stanford University.
+3. Neither the name of the copyright holder nor the names of its contributors
+may be used to endorse or promote products derived from this software without
+specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
  */
 package org.lockss.laaws.mdx;
@@ -32,6 +36,9 @@ import org.lockss.app.LockssApp;
 import org.lockss.app.LockssApp.AppSpec;
 import org.lockss.app.LockssApp.ManagerDesc;
 import org.lockss.app.LockssDaemon;
+import org.lockss.metadata.extractor.MetadataExtractorManager;
+import org.lockss.metadata.extractor.job.JobDbManager;
+import org.lockss.metadata.extractor.job.JobManager;
 import org.lockss.rs.base.BaseSpringBootApplication;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -59,9 +66,14 @@ public class MdxApplication extends BaseSpringBootApplication
     METADATA_DB_MANAGER_DESC,
     // start metadata manager after pluggin manager and database manager.
     METADATA_MANAGER_DESC,
+    new ManagerDesc(LockssDaemon.managerKey(MetadataExtractorManager.class),
+	"org.lockss.metadata.extractor.MetadataExtractorManager"),
+    // Start the job database manager.
+    new ManagerDesc(LockssDaemon.managerKey(JobDbManager.class),
+	"org.lockss.metadata.extractor.job.JobDbManager"),
     // Start the job manager.
-    JOB_DB_MANAGER_DESC,
-    JOB_MANAGER_DESC,
+    new ManagerDesc(LockssDaemon.managerKey(JobManager.class),
+	"org.lockss.metadata.extractor.job.JobManager"),
     // Start the COUNTER reports manager.
     COUNTER_REPORTS_MANAGER_DESC,
     // NOTE: Any managers that are needed to decide whether a servlet is to be
@@ -94,9 +106,10 @@ public class MdxApplication extends BaseSpringBootApplication
     // Check whether there are command line arguments available.
     if (args != null && args.length > 0) {
       // Yes: Start the LOCKSS daemon.
-      logger.info("Starting the LOCKSS daemon");
+      logger.info("Starting the LOCKSS Metadata Extractor Service");
+
       AppSpec spec = new AppSpec()
-	.setName("Metadate Extractor Service")
+	.setName("Metadata Extractor Service")
 	.setArgs(args)
 	.addAppConfig(LockssDaemon.PARAM_START_PLUGINS, "true")
 	.setAppManagers(myManagerDescs);
