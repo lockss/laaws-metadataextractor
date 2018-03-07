c27e318e6b712e79c5d9f957e46034ca30dcca28
fergaloy <fergaloy@stanford.edu>
2018-03-07 10:47:24 -1000

    Fixed documentation and added status client.

---
+++ src/main/java/org/lockss/laaws/mdx/client/GetStatusClient.java
@@ -0,0 +1,62 @@
+/*
+
+ Copyright (c) 2018 Board of Trustees of Leland Stanford Jr. University,
+ all rights reserved.
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ STANFORD UNIVERSITY BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+ IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ Except as contained in this notice, the name of Stanford University shall not
+ be used in advertising or otherwise to promote the sale, use or other dealings
+ in this Software without prior written authorization from Stanford University.
+
+ */
+package org.lockss.laaws.mdx.client;
+
+import java.net.URI;
+import org.lockss.rs.status.ApiStatus;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
+
+public class GetStatusClient extends BaseClient {
+
+  public static void main(String[] args) throws Exception {
+    String template = baseUri + "/status";
+
+    // Create the URI of the request to the REST service.
+    UriComponents uriComponents =
+	UriComponentsBuilder.fromUriString(template).build();
+
+    UriComponentsBuilder builder =
+	UriComponentsBuilder.newInstance().uriComponents(uriComponents);
+
+    URI uri = builder.build().encode().toUri();
+    System.out.println("uri = " + uri);
+
+    ResponseEntity<ApiStatus> response = getRestTemplate().exchange(uri,
+	HttpMethod.GET, new HttpEntity<String>(null, getHttpHeaders()),
+	ApiStatus.class);
+
+    int status = response.getStatusCodeValue();
+    System.out.println("status = " + status);
+    ApiStatus result = response.getBody();
+    System.out.println("result = " + result);
+  }
+}
