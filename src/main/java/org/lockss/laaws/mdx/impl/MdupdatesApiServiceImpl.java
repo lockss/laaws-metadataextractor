0f395d4e90a8f78be055c3ed9f712aaaf0ab1b52
fergaloy <fergaloy@stanford.edu>
2019-04-08 07:44:47 -1000

    Updated the ApiStatus-related call.

--- src/main/java/org/lockss/laaws/mdx/impl/MdupdatesApiServiceImpl.java
+++ src/main/java/org/lockss/laaws/mdx/impl/MdupdatesApiServiceImpl.java
@@ -1,6 +1,6 @@
 /*
 
-Copyright (c) 2000-2018 Board of Trustees of Leland Stanford Jr. University,
+Copyright (c) 2000-2019 Board of Trustees of Leland Stanford Jr. University,
 all rights reserved.
 
 Redistribution and use in source and binary forms, with or without modification,
@@ -351,8 +351,6 @@ public class MdupdatesApiServiceImpl extends SpringLockssBaseApiController
     }
   }
 
-  private static final String API_VERSION = "1.0.0";
-
   /**
    * Provides the status object.
    * 
@@ -360,8 +358,7 @@ public class MdupdatesApiServiceImpl extends SpringLockssBaseApiController
    */
   @Override
   public ApiStatus getApiStatus() {
-    return new ApiStatus()
-      .setVersion(API_VERSION)
+    return new ApiStatus("swagger/swagger.yaml")
       .setReady(LockssApp.getLockssApp().isAppRunning());
   }
 
